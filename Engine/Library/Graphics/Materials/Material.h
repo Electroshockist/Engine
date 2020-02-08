68c92b92c739035aec8eb095b51d035efa55b5bd
Electroshockist <ericonfire2@yahoo.com>
2020-02-07 19:28:04 -0500

    Updated material
    Updated dynamic properties on AMaterial

--- Engine/Library/Graphics/Materials/Material.h
+++ Engine/Library/Graphics/Materials/Material.h
@@ -1,13 +1,37 @@
 #ifndef MATERIAL_H
-class Material{
-public:
-	Material(){}
+#define MATERIAL_H
 
-	~Material(){}
+#include <string>
+#include <map>
 
-private:
-
+#include <glm/glm.hpp>
+union Parameter{
+	float f;
+	double d;
+	int i;
+	unsigned int uint;
+	glm::vec2 vec2;
+	glm::vec3 vec3;
+	glm::vec4 vec4;
 };
 
-#endif // !MATERIAL_H
+class Material{
 
+
+	Material(std::string name, Parameter parameter){
+		AddParameter(name, parameter);
+	}
+
+	void AddParameter(std::string name, Parameter parameter){
+		parameters[name] = parameter;
+	}
+
+	Parameter *getParameter(std::string name){
+		return &parameters.at(name);
+	}
+
+private:
+	typedef std::map<std::string, Parameter> Parameters;
+	Parameters parameters;
+};
+#endif // !MATERIAL_H
