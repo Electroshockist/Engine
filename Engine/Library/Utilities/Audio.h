52370bbed89d7f8f61a7d5441d8bb715b2dd9d01
INNERSPACE3000 <45610158+INNERSPACE3000@users.noreply.github.com>
2020-03-19 17:47:06 -0400

    Added Audio classes
    Audio Classes have been added. Needs to be checked.

---
+++ Engine/Library/Utilities/Audio.h
@@ -0,0 +1,29 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+#include <SDL_mixer.h>
+#include <map>
+#include <string>
+class Audio {
+
+private:
+
+	std::map<std::string, Mix_Music*> mMusic;
+	std::map<std::string, Mix_Chunk*> mSFX;
+
+public:
+
+	Mix_Music* GetMusic(std::string filename);
+	Mix_Chunk* GetSFX(std::string filename);
+
+
+
+private:
+
+		Audio();
+		~Audio();
+
+};
+
+
+#endif
+
