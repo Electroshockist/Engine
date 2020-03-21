eea08cacbabd2b23eac36a5f5f04d5710364dc72
Electroshockist <ericonfire2@yahoo.com>
2020-03-20 21:01:21 -0400

    Updated AudioManager

--- Engine/Library/Audio/AudioManager.cpp
+++ Engine/Library/Audio/AudioManager.cpp
@@ -1,19 +1,19 @@
 #include "AudioManager.h"
 
-AudioManager* AudioManager::sInstance = nullptr;
+AudioManager* AudioManager::managerInstance = nullptr;
 
 AudioManager* AudioManager::Instance(){
 
-	if(sInstance == NULL)
-		sInstance = new AudioManager();
+	if(managerInstance == NULL)
+		managerInstance = new AudioManager();
 
-	return sInstance;
+	return managerInstance;
 }
 
 void AudioManager::Release(){
 
-	delete sInstance;
-	sInstance = NULL;
+	delete managerInstance;
+	managerInstance = NULL;
 
 }
 
@@ -44,18 +44,13 @@ void AudioManager::PlayMusic(std::string filename, int loops){
 void AudioManager::PauseMusic(){
 	if(Mix_PlayChannel() != 0)
 		Mix_PauseMusic();
-
-
 }
 
 void AudioManager::ResumeMusic(){
 	if(Mix_PausedMusic() != 0)
 		Mix_ResumeMusic();
-
 }
 
 void AudioManager::PlaySFX(std::string filename, int loops, int channel){
-
 	Mix_PlayChannel(channel, mAssetMgr->GetSFX(filename), loops);
-
 }
--- Engine/Library/Audio/AudioManager.h
+++ Engine/Library/Audio/AudioManager.h
@@ -4,7 +4,7 @@
 #include <string>
 
 class AudioManager {
-	static AudioManager* sInstance;
+	static AudioManager* managerInstance;
 
 	Audio* mAssetMgr;
 
@@ -16,12 +16,14 @@ public:
 	static AudioManager* Instance();
 	static void Release();
 
-	void PlayMusic(std::string filename, int loops = -1);
+	void PlaySFX(std::string filename, int loops = 0, int channel = 0);
 
+	void PlayMusic(std::string filename, int loops = -1);
 	void PauseMusic();
 	void ResumeMusic();
 
-	void PlaySFX(std::string filename, int loops = 0, int channel = 0);
+	Mix_Music* GetMusic(std::string filename);
+	Mix_Chunk* GetSFX(std::string filename);
 };
 #endif // !AUDIO_H
 
