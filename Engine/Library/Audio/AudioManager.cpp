80423166710d8f79506f6ac9be3e2bbc3e42338e
Electroshockist <ericonfire2@yahoo.com>
2020-05-26 17:06:01 -0400

    Updated AudioManager
    Also Moved Text folder to proper place

--- Engine/Library/Audio/AudioManager.cpp
+++ Engine/Library/Audio/AudioManager.cpp
@@ -3,6 +3,7 @@
 #include <SDL\SDL_filesystem.h>
 
 #include "../Utility/FilePathParser.h"
+#include "../Debugging/Debug.h"
 
 AudioManager* AudioManager::mgrInstance = NULL;
 
@@ -14,7 +15,7 @@ Mix_Music* AudioManager::GetMusic(std::string filename){
 
 		mMusic[fullpath] = Mix_LoadMUS(fullpath.c_str());
 		if (mMusic[fullpath] == NULL) {
-			printf("Music Loading Error: File-%s Error-%s", filename.c_str(), Mix_GetError());
+			Debug::error("Music Loading Error: File-" + filename + " Error-%s" + Mix_GetError(), __FILE__, __LINE__);
 		}
 		return mMusic[fullpath];
 	}
@@ -29,7 +30,7 @@ Mix_Chunk* AudioManager::GetSFX(std::string filename)
 
 		mSFX[fullpath] = Mix_LoadWAV(fullpath.c_str());
 		if (mSFX[fullpath] == NULL) {
-			printf("SFX Loading Error: File-%s Error-%s", filename.c_str(), Mix_GetError());
+			Debug::error("SFX Loading Error : File-" + filename + " Error-%s" + Mix_GetError(), __FILE__, __LINE__);
 		}
 		return mSFX[fullpath];
 	}
